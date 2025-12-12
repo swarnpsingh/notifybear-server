208a0562e6d0b0ec43663f627c4a1609c6ea0e74
amitabh-anandpd <amitabh.anandpd@gmail.com>
2025-12-12 16:53:09 +0530

    Corrected token refresh url

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("me/", me, name="me"),
 ]
