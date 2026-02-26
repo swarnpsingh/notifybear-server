11fea313393060a9024ee359b60ad65d8ee5285a
amitabh-anandpd <amitabh.anandpd@gmail.com>
2026-02-27 02:29:36 +0530

    Middleware removed

--- NotifybearServer/settings.py
+++ NotifybearServer/settings.py
@@ -112,7 +112,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ml.middleware.DeleteTempFileMiddleware',
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
