649954d886f45be4664d53efb012cc046241e1e0
amitabh-anandpd <amitabh.anandpd@gmail.com>
2026-03-22 12:52:14 +0530

    Email backed settings added

--- NotifybearServer/settings.py
+++ NotifybearServer/settings.py
@@ -210,3 +210,15 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
