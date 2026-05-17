ace67322cdd223d3a646e4e84587902df42646b4
amitabh-anandpd <amitabh.anandpd@gmail.com>
2026-05-18 01:57:14 +0530

    Add TrainingFeature model for synced mobile ML features

---
+++ ml/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from Accounts.models import User
+
+
+class TrainingFeature(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    feature_version = models.IntegerField(default=1)
+
+    features = models.JSONField()
+
+    label = models.FloatField(null=True, blank=True)
+
+    notification_key = models.CharField(max_length=512)
+
+    package_name = models.CharField(max_length=256)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    synced_at = models.DateTimeField(auto_now_add=True)
+
+    used_for_training = models.BooleanField(default=False)
