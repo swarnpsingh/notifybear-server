f5e4ded87ac96d3f2f90cff544f5fcf58f1f8ace
amitabh-anandpd <amitabh.anandpd@gmail.com>
2026-05-24 00:40:12 +0530

    Added admin for new model in ml app

---
+++ ml/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from .models import TrainingFeature
+
+
+@admin.register(TrainingFeature)
+class TrainingFeatureAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "user",
+        "package_name",
+        "feature_version",
+        "label",
+        "used_for_training",
+        "created_at",
+    )
+
+    list_filter = (
+        "feature_version",
+        "used_for_training",
+        "created_at",
+    )
+
+    search_fields = (
+        "user__username",
+        "package_name",
+        "notification_key",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "synced_at",
+    )
+
+    ordering = ("-created_at",)
+
+    list_per_page = 50
